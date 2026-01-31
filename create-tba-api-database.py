741cdfe51757a2a1a660cc4ef42e4f53dbd82501
Mike Silva <mike.a.silva@gmail.com>
2026-01-31 09:23:18 -0500

    More work to builld the database

--- create-tba-api-database.py
+++ create-tba-api-database.py
@@ -3,9 +3,9 @@ import sqlite3
 import json
 import pandas as pd
 
-conn = sqlite3.connect("tba-api.db")
+conn = sqlite3.connect("trueskill.db")
 cursor = conn.cursor()
-comp_levels = {"qm":1, "qf":2, "sf":3, "f":4, "ef":5}
+comp_levels = {"qm": 1, "ef": 2, "qf": 3, "sf": 4, "f": 5}
 
 cursor.execute('DROP TABLE IF EXISTS "tba_events"')
 cursor.execute(
@@ -13,7 +13,7 @@ cursor.execute(
 )
 cursor.execute('DROP TABLE IF EXISTS "tba_matches"')
 cursor.execute(
-    'CREATE TABLE "tba_matches" ( id INTEGER PRIMARY KEY, "key" TEXT, "event_key" TEXT, "match_number" INTEGER, "comp_level" TEXT, "set_number" INTEGER, "blue_score" INTEGER, "blue_team_1" TEXT, "blue_team_2" TEXT, "blue_team_3" TEXT, "blue_team_4" TEXT, "red_score" INTEGER, "red_team_1" TEXT, "red_team_2" TEXT, "red_team_3" TEXT, "red_team_4" TEXT )'
+    'CREATE TABLE "tba_matches" ( id INTEGER PRIMARY KEY, "key" TEXT, "event_key" TEXT, "match_number" INTEGER, "comp_level" TEXT, "set_number" INTEGER, "winning_alliance" TEXT, "blue_score" INTEGER, "blue_team_1" TEXT, "blue_team_2" TEXT, "blue_team_3" TEXT, "blue_team_4" TEXT, "red_score" INTEGER, "red_team_1" TEXT, "red_team_2" TEXT, "red_team_3" TEXT, "red_team_4" TEXT )'
 )
 conn.commit()
 
@@ -32,7 +32,7 @@ tba_events_columns = [
     "postal_code",
 ]
 for file_path in sorted(glob("raw_data/tba_events/*.json")):
-    #print(file_path)
+    # print(file_path)
     with open(file_path, "r") as json_file:
         # data = json.load(json_file)
         data = pd.read_json(json_file)[tba_events_columns].sort_values(by="start_date")
@@ -42,7 +42,7 @@ for file_path in sorted(glob("raw_data/tba_events/*.json")):
 
 data = []
 for file_path in sorted(glob("raw_data/tba_matches/*.json")):
-    #print(file_path)
+    # print(file_path)
     with open(file_path, "r") as json_file:
         for temp in json.load(json_file):
             row = {
@@ -51,18 +51,20 @@ for file_path in sorted(glob("raw_data/tba_matches/*.json")):
                 "match_number": temp["match_number"],
                 "comp_level": temp["comp_level"],
                 "set_number": temp["set_number"],
-                "match_type": comp_levels[temp["comp_level"]]
+                "match_type": comp_levels[temp["comp_level"]],
+                "winning_alliance": temp["winning_alliance"],
             }
             for alliance in temp["alliances"].keys():
                 row[f"{alliance}_score"] = temp["alliances"][alliance]["score"]
                 for i, team_key in enumerate(temp["alliances"][alliance]["team_keys"]):
-                    row[f"{alliance}_team_{i+1}"] = team_key
+                    row[f"{alliance}_team_{i + 1}"] = team_key
             data.append(row)
-data = pd.DataFrame(data).sort_values(by=["event_key", "match_type", "match_number", "set_number"]).drop("match_type", axis=1)
+data = (
+    pd.DataFrame(data)
+    .sort_values(by=["event_key", "match_type", "match_number", "set_number"])
+    .drop("match_type", axis=1)
+)
 data.to_sql("tba_matches", conn, if_exists="append", index=False)
+
 conn.commit()
-
 conn.close()
-
-#print(data)
-#print(sorted(comp_level))
